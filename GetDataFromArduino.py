c77bf5be58a6298927c70e3ed591e79a11c6509d
Gymron <97519752+Gymron@users.noreply.github.com>
2022-01-18 22:14:28 +0000

    Add files via upload

--- GetDataFromArduino.py
+++ GetDataFromArduino.py
@@ -4,12 +4,14 @@ line = "18.36+22.00+35.00"
 
 
 def GetData():
-#    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-#    ser.reset_input_buffer()
-#    ser.write(b'data')
-#    ser.close()
-#    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-#    if ser.in_waiting > 0:
-#        line = ser.readline().decode('utf-8').rstrip()
-#        print(line)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+    ser.write(b'data')
+    ser.close()
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        with open('readme.txt', 'w') as f:
+            f.write("current SensorData:", line)
     return line
